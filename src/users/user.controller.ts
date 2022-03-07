9d4960665a129128f1f1d2e361a1b2e6107abd63
Maksudkhanov <azik.maksudxanov.am@gmail.com>
2022-03-07 19:38:30 +0500

    added errorHandler

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { BaseController } from "../common/base.controller";
+import { HTTPError } from "../common/errors/http-error.class";
 import { LoggerService } from "../logger/logger.service";
 
 export class UserController extends BaseController {
@@ -12,10 +13,12 @@ export class UserController extends BaseController {
     }
 
     login(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'login')
+        // this.ok(res, 'login')
+        next(new HTTPError(401, 'Register Error', 'Login'))
     }
 
     register(req: Request, res: Response, next: NextFunction) {
-        this.ok(res, 'register')
+        // this.ok(res, 'register')
+        
     }
 }     
