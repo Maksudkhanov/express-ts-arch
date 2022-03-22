590c7a61c53b082eba6af2330c91f7a6d8367624
Maksudkhanov <azik.maksudxanov.am@gmail.com>
2022-03-22 23:31:15 +0500

    added inversify js

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,20 @@
 import { NextFunction, Request, Response } from "express";
+import { inject, injectable } from "inversify";
 import { BaseController } from "../common/base.controller";
 import { HTTPError } from "../common/errors/http-error.class";
+import { ILogger } from "../logger/logger.interface";
 import { LoggerService } from "../logger/logger.service";
+import { TYPES } from "../types";
+import 'reflect-metadata'
+import { IUserController } from "./users.controller.interface";
 
-export class UserController extends BaseController {
-    constructor(logger: LoggerService) {
-        super(logger)
+
+@injectable()
+export class UserController extends BaseController implements IUserController{
+    constructor(
+         @inject(TYPES.ILogger) private loggerService: ILogger
+         ) {
+        super(loggerService)
         this.bindRoutes([
             {path: '/login', method: 'post', func: this.login},
             {path: '/register', method: 'post', func: this.register}
