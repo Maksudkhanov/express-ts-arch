e8a08af6f7db8ef53f52b04b5d0513d8062db658
Maksudkhanov <azik.maksudxanov.am@gmail.com>
2022-04-03 23:14:57 +0500

    added userService

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { IExceptionFilter } from './common/errors/exception.filter.interface';
 import { ILogger } from './logger/logger.interface';
 import { LoggerService } from './logger/logger.service';
 import { TYPES } from './types';
+import { UserService } from './users/user.service';
+import { IUserService } from './users/user.service.interface';
 import { UserController } from './users/users.controller';
 import { IUserController } from './users/users.controller.interface';
 
@@ -12,6 +14,7 @@ export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
 	bind<ILogger>(TYPES.ILogger).to(LoggerService);
 	bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
 	bind<IUserController>(TYPES.UserController).to(UserController);
+	bind<IUserService>(TYPES.UserService).to(UserService);
 	bind<App>(TYPES.Application).to(App);
 });
 
