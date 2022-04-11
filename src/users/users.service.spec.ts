adee96bd07e69425b70ebafa24036df9904abb34
Maksudkhanov <azik.maksudxanov.am@gmail.com>
2022-04-12 02:14:19 +0500

    added jest unit testing

---
+++ src/users/users.service.spec.ts
@@ -0,0 +1,83 @@
+import 'reflect-metadata';
+import { UserModel } from '@prisma/client';
+import { Container } from 'inversify';
+import { IConfigService } from '../config/config.service.interface';
+import { TYPES } from '../types';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+import { IUserService } from './user.service.interface';
+import { IUsersRepository } from './users.repository.interface';
+
+const ConfigServiceMock: IConfigService = {
+	get: jest.fn(),
+};
+
+const UsersRepositoryMock: IUsersRepository = {
+	find: jest.fn(),
+	create: jest.fn(),
+};
+
+const container = new Container();
+let usersRepository: IUsersRepository;
+let configService: IConfigService;
+let usersService: IUserService;
+
+beforeAll(() => {
+	container.bind<IUserService>(TYPES.UserService).to(UserService);
+	container.bind<IConfigService>(TYPES.ConfigService).toConstantValue(ConfigServiceMock);
+	container.bind<IUsersRepository>(TYPES.UsersRepository).toConstantValue(UsersRepositoryMock);
+
+	usersService = container.get<IUserService>(TYPES.UserService);
+	configService = container.get<IConfigService>(TYPES.ConfigService);
+	usersRepository = container.get<IUsersRepository>(TYPES.UsersRepository);
+});
+
+let createdUser: UserModel | null;
+
+describe('User Service', () => {
+	it('createUser', async () => {
+		configService.get = jest.fn().mockReturnValueOnce('1');
+		usersRepository.create = jest.fn().mockImplementationOnce(
+			(user: User): UserModel => ({
+				name: user.name,
+				email: user.email,
+				password: user.password,
+				id: 1,
+			}),
+		);
+		createdUser = await usersService.createUser({
+			email: 'maksudkhanov@mail.ru',
+			name: 'Saidazizkhon',
+			password: 'coca-cola',
+		});
+		expect(createdUser?.id).toEqual(1);
+		expect(createdUser?.password).not.toEqual('1');
+	});
+
+	it('validateUser - success', async () => {
+		usersRepository.find = jest.fn().mockReturnValueOnce(createdUser);
+		const res = await usersService.validateUser({
+			email: 'maksudkhanov@mail.ru',
+			password: 'coca-cola',
+		});
+		expect(res).toBeTruthy();
+	});
+
+	it('validateUser - wrong password', async () => {
+		usersRepository.find = jest.fn().mockReturnValueOnce(createdUser);
+		const res = await usersService.validateUser({
+			email: 'maksudkhanov@mail.ru',
+			password: 'coca-cola2',
+		});
+		expect(res).toBeFalsy();
+	});
+
+	it('validateUser - wrong user', async () => {
+		usersRepository.find = jest.fn().mockReturnValueOnce(null);
+		const res = await usersService.validateUser({
+			email: 'maksudkhanov2@mail.ru',
+			password: 'coca-cola2',
+		});
+		expect(res).toBeFalsy();
+	});
+});
